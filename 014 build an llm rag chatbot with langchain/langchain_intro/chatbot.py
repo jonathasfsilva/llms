b20138b7d79b14c1dbee1ed5e3ee4303755c7227
jonathasfsilva <jonathas_ssilva@hotmail.com>
2025-09-17 16:37:16 -0300

    objetos de recuperacao

--- 014 build an llm rag chatbot with langchain/langchain_intro/chatbot.py
+++ 014 build an llm rag chatbot with langchain/langchain_intro/chatbot.py
@@ -6,8 +6,14 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.schema.runnable import RunnablePassthrough
 
 dotenv.load_dotenv()
+
+REVIEWS_CHROMA_PATH = "chroma_data/"
 
 review_template_str = """Seu trabalho é usar avaliações de pacientes para responder a perguntas sobre a experiência deles em um hospital. Use o contexto a seguir para responder às perguntas. Seja o mais detalhado possível, mas não invente nenhuma informação que não esteja no contexto. Se você não souber uma resposta, diga que não sabe.
 
@@ -36,4 +42,18 @@ review_prompt_template = ChatPromptTemplate(
 
 chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
-review_chain = review_prompt_template | chat_model
+output_parser = StrOutputParser()
+
+reviews_vector_db = Chroma(
+    persist_directory=REVIEWS_CHROMA_PATH,
+    embedding_function=OpenAIEmbeddings()
+)
+
+reviews_retriever  = reviews_vector_db.as_retriever(k=10)
+
+review_chain = (
+    {"context": reviews_retriever, "question": RunnablePassthrough()}
+    | review_prompt_template
+    | chat_model
+    | StrOutputParser()
+)
