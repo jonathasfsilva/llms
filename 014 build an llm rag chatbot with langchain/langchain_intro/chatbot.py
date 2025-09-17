6621007601d5c662c68f33dd6cd37be1a1bfd3d3
jonathasfsilva <jonathas_ssilva@hotmail.com>
2025-09-17 14:49:29 -0300

    modelo chat e prompt

--- 014 build an llm rag chatbot with langchain/langchain_intro/chatbot.py
+++ 014 build an llm rag chatbot with langchain/langchain_intro/chatbot.py
@@ -1,7 +1,39 @@
-
 import dotenv
 from langchain_openai import ChatOpenAI
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
 
 dotenv.load_dotenv()
 
-chat_model = ChatOpenAI(model_name="gpt-3.5-turbo")
+review_template_str = """Seu trabalho é usar avaliações de pacientes para responder a perguntas sobre a experiência deles em um hospital. Use o contexto a seguir para responder às perguntas. Seja o mais detalhado possível, mas não invente nenhuma informação que não esteja no contexto. Se você não souber uma resposta, diga que não sabe.
+
+{context}
+"""
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["context"],
+        template=review_template_str,
+    )
+)
+
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["question"],
+        template="{question}",
+    )
+)
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt_template = ChatPromptTemplate(
+    input_variables=["context", "question"],
+    messages=messages,
+)
+
+chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+
+review_chain = review_prompt_template | chat_model
